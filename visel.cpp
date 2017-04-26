e1f25266fc10c1d9ef2e54416e29d0a901cf1943
VagueFuture <igrokart97@mail.ru>
2017-04-26 20:19:48 +0700

    New menu Kim_Artem \(-_-)/

--- visel.cpp
+++ visel.cpp
@@ -3,95 +3,103 @@
 #include <ctime>
 #include <graphics.h>
 #include <string.h>
+#include <windows.h>
 
-int autor ()  {
-	initwindow(600,800); 
-	outtextxy(15,10,"Autor:"); 
+
+void autor ()  {
+	
+initwindow (800,800);
+
+outtextxy(15,10,"Autors:"); 
+	 settextstyle(0,0,1);
  outtextxy(15,25,"Anastasiya Yun, Kim Artem, Raspopin Aleksandr"); 
+ 
+  
+     settextstyle(0,0,10);
+     setcolor(15);
+     outtextxy (300,200,"Hangman");
+     settextstyle(9,0,3);
+     outtextxy (295,300,"Enter");
+      settextstyle(9,0,3);
+	
  	
-return 0;	
+//return 0;	
 }
 
 int swit (int k)  {
+	printf("Case== %d",k);
+	cleardevice();
 
     switch (k){
-			case 1: moveto(30,650); 
-	lineto(450,650); getch(); break;
-			case 2: moveto(50,680);         
-lineto(50,50); getch();break;
-			case 3: moveto(50,50);
-lineto(480,50);getch(); break;
-			case 4: moveto(480,50);
-lineto(480,200);getch(); break;
-			case 5: circle(480,250,50);getch(); break;
-			case 6: circle(480,270,30);getch(); break;
-			case 7: moveto(480,300);
-lineto(480,400); getch();break;
-			case 8: moveto(480,350);
-lineto(380,370);getch(); break;
-			case 9: moveto(480,350);
-lineto(580,370);getch(); break;
-			case 10: moveto(480,400);
-lineto(450,480);getch(); break;
-			case 11: moveto(480,400);
-lineto(500,480);
-bar(800,800,0,0);
-outtextxy(250,300,"GAME OVER"); break;
+			case 1:setcolor(6);  moveto(30,650); lineto(450,650);  break;
+			case 2:setcolor(6); moveto(30,650); lineto(450,650); moveto(50,680); lineto(50,50); break;
+			case 3:setcolor(6);  moveto(30,650); lineto(450,650); moveto(50,680); lineto(50,50); moveto(50,50); lineto(480,50); break;
+			case 4:setcolor(6);  moveto(30,650); lineto(450,650); moveto(50,680); lineto(50,50); moveto(50,50); lineto(480,50); moveto(480,50); lineto(480,200); break;
+			case 5:setcolor(6); moveto(30,650); lineto(450,650); moveto(50,680); lineto(50,50); moveto(50,50); lineto(480,50); moveto(480,50); lineto(480,200); circle(480,250,50); break;
+			case 6:setcolor(6); moveto(30,650); lineto(450,650); moveto(50,680); lineto(50,50); moveto(50,50); lineto(480,50); moveto(480,50); lineto(480,200); circle(480,250,50); circle(480,270,30); break;
+			case 7:setcolor(6);moveto(30,650); lineto(450,650); moveto(50,680); lineto(50,50); moveto(50,50); lineto(480,50); moveto(480,50); lineto(480,200); circle(480,250,50); circle(480,270,30); moveto(480,300); lineto(480,400); break;
+			case 8:setcolor(6); moveto(30,650); lineto(450,650); moveto(50,680); lineto(50,50); moveto(50,50); lineto(480,50); moveto(480,50); lineto(480,200); circle(480,250,50); circle(480,270,30); moveto(480,300); lineto(480,400); moveto(480,350); lineto(380,370); break;
+			case 9:setcolor(6);  moveto(30,650); lineto(450,650); moveto(50,680); lineto(50,50); moveto(50,50); lineto(480,50); moveto(480,50); lineto(480,200); circle(480,250,50); circle(480,270,30); moveto(480,300); lineto(480,400); moveto(480,350); lineto(380,370); moveto(480,350); lineto(580,370); break;
+			case 10:setcolor(6); moveto(30,650); lineto(450,650); moveto(50,680); lineto(50,50); moveto(50,50); lineto(480,50); moveto(480,50); lineto(480,200); circle(480,250,50); circle(480,270,30); moveto(480,300); lineto(480,400); moveto(480,350); lineto(380,370); moveto(480,350); lineto(580,370); moveto(480,400); lineto(450,480); break;
+			case 11:  moveto(480,400); lineto(500,480); bar(800,800,0,0); outtextxy(250,300,"GAME OVER"); break;
 			default: break;
 		}
-		return 0;
+	
+		//return 0;
 }
 
 
 int main(){
+	int l;
 autor();
-getch();
-
+//Sleep(1000);
+l=getch();
+if (l==13)
+{
  //closegraph();
- 
+ cleardevice();
  char s[80],ch[2];
  char vvod[2];
-
  gets(s);
+ system("cls");
+ 
 
- int i=0,t=0,r=0,n=0,y=0;
+ int j=0,i=0,t=0,r=0,n=0,y=0;
  //initwindow(600,800); 
  
-while(r!=11){
+ while(s[j]!='\0'){
+	 j++;
+}
+char otvet[j];
 
+ printf("Chislo bukv %d \n",j);
+while(r!=11){
 gets(vvod);
- //puts(vvod);
  t=0;
  while(s[i]!='\0')
  {	 y++;
  	if (s[i]==vvod[0])
  	{
  	printf("%s",vvod);
-	 t++;
-	 n++;		
+	 t++; 
+	 s[i]=otvet[i];
 	 printf("\n");	
  	}
 	 i++;
 	
 }
-printf("Bookva %s est & povtorayenca %d raza" ,vvod,t);	
+printf("Bookva %s povtorayenca %d raz\n" ,vvod,t);	
 //////////
 
- if (t<1)
+ if (t==0)
  r++;
- //////////////
- 
  	i=0;
- 	//printf("\n r= %d",r);
  	swit(r);
- 	if (n=y)
- 	{
- 		bar(800,800,0,0);
-outtextxy(250,300,"YOU WIN");
-getch();
- 	}
+ 	
+ 
 }
 getch();
 closegraph();
 return 0;
+}
 }

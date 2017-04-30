0165ec6570ad17dee3096f7ea840d7cc4f12855a
VagueFuture <igrokart97@mail.ru>
2017-04-30 22:26:26 +0700

    Practicheski_Rabochaya_versiya ~(øUø)~

--- visel.cpp
+++ visel.cpp
@@ -8,29 +8,34 @@
 void stand1 () { setfillstyle(1,6); bar(30,650,500,700); }
 void stand2 () {setfillstyle(1,6); bar(50,680,100,100); }
 void stand3 () {setfillstyle(1,6); bar(100,100,480,150); }
-void ver() {moveto(480,150); lineto(480,200); }
+void ver() {setcolor(15); moveto(450,150); lineto(450,300); }
 void head () {setcolor(6); circle(470,300,20); }
 void telo () {setcolor(6); moveto(450,300); lineto(450,400); }
 void rukal () {setcolor(6); moveto(450,305); lineto(445,350);moveto(445,350); lineto(455,370); }
 void rukar () {setcolor(6); moveto(450,305); moveto(450,310); lineto(470,330);moveto(450,320); lineto(470,330); }
 void legl () {moveto(450,400); lineto(460,450);moveto(460,450); lineto(460,480); }
 void legr () {moveto(450,400); lineto(470,350);moveto(470,350); lineto(470,410); }
-void GameOver () {bar(800,800,0,0); outtextxy(250,300,"GAME OVER"); }
-
-
+void GameOver () { setfillstyle(1,3);
+ bar(0,0,800,800);
+settextstyle(0,0,15); setcolor(8); outtextxy(250,300,"GAME OVER"); }
+void YouWin () {cleardevice(); setfillstyle(1,3);
+ bar(0,0,800,800); settextstyle(0,0,15);
+     setcolor(9); outtextxy(250,300,"You WIN"); }
 void HUD ()  {
 	
  cleardevice();
  setcolor(RED);
   setfillstyle(1,3);
  bar(0,0,800,800);
+////////////
 
+   //rectangle(30,795,60,795);
 //return 0;	
 }
-
 void autor ()  {
 	
 initwindow (800,800);
+setbkcolor(3);
 setfillstyle(1,3);
  bar(0,0,800,800);
  Sleep(1000);
@@ -45,112 +50,137 @@ outtextxy(15,10,"Autors:");
  Sleep(1000);
   
    settextstyle(0,0,10);
- outtextxy(300,100,"PRESENT");
+ outtextxy(300,300,"PRESENT");
  Sleep(500);
      settextstyle(0,0,10);
      setcolor(15);
-     outtextxy (300,200,"Hangman");
+     outtextxy (400,400,"Hangman");
      Sleep(1000);
      settextstyle(9,0,3);
-     outtextxy (295,300,"Press Enter");
+     outtextxy (295,500,"Press Enter");
       settextstyle(9,0,3);
 	
  	
 //return 0;	
 }
-
 int swit (int k)  {
-	setfillstyle(1,3);
- bar(0,0,800,800);
+	//setfillstyle(1,3);
+// bar(0,0,800,800);
 	printf("Case== %d",k);
 	
 	
 	switch (k){
 	
 			case 1:stand1 ();  break;
-			case 2:stand2 (); break;
-			case 3:stand3 ();  break;
-			case 4:ver(); break;
-			case 5:head(); break;
-			case 6:telo(); break;
-			case 7:rukal(); break;
-			case 8:rukar(); break;
-			case 9:legl(); break;
-			case 10:legr(); break;
-			case 11:GameOver (); break;
+			case 2:stand1 (); stand2 (); break;
+			case 3:stand1 (); stand2 (); stand3 ();  break;
+			case 4:stand1 (); stand2 (); stand3 (); ver(); break;
+			case 5:stand1 (); stand2 (); stand3 (); ver(); head(); break;
+			case 6:stand1 (); stand2 (); stand3 (); ver(); head(); telo(); break;
+			case 7:stand1 (); stand2 (); stand3 (); ver(); head(); telo(); rukal(); break;
+			case 8:stand1 (); stand2 (); stand3 (); ver(); head(); telo(); rukal(); rukar(); break;
+			case 9:stand1 (); stand2 (); stand3 (); ver(); head(); telo(); rukal(); rukar(); legl(); break;
+			case 10:stand1 (); stand2 (); stand3 (); ver(); head(); telo(); rukal(); rukar(); legl(); legr(); break;
+			case 11:GameOver(); break;
 			default: break;
 		}
-		
-		/*if(k==1) stand1 ();
-		if(k==2) stand2();
-		if(k==3) stand3();
-		if(k==4) ver();
-		if(k==5) head();
-		if(k==6) telo();
-		if(k==7) rukal();
-		if(k==8) rukar();
-		if(k==9) legl();
-		if(k==10) GameOver ();
-	*/	
-		//return 0;
+	
 }
 
-
 int main(){
-	int l;
+	int Enter,Chislo_simvolov=0;
 autor();
 
-//Sleep(1000);
-l=getch();
-if (l==13)
-{
- //closegraph();
+Enter=getch();
+if (Enter==13)
+{ 
 HUD();
- char s[80],ch[2];
+
  char vvod[2];
- 
- gets(s);
+ char Text[80]={"интерстеллар"};
+
  system("cls");
  
-
- int j=0,i=0,t=0,r=0,n=0,y=0;
- //initwindow(600,800); 
- 
- while(s[j]!='\0'){
-	 j++;
+ int j=0,i=0,t=0,nevernii_otvet=0,n=0,y=0;
+  
+ while(Text[j]!='\0'){
+	 j++;	 
 }
 char otvet[j];
 
- printf("Chislo bukv %d \n",j);
-while(r!=11){
-	swit(r);
-gets(vvod);
+printf("Chislo bukv %d \n",j);
+ 
+ //////////////////////////////
+ int wight;
+ wight=0;
+ for (i=0;i<j;i++)
+ {
+ //setcolor(20);
+ //rectangle(30+wight,755,60+wight,715);
+ //wight+=30;
+ otvet[i]='_';
+}
+ /////////////////////////////
+ nevernii_otvet=0;
+while(nevernii_otvet!=12){
+	swit(nevernii_otvet);
+	vvod[0]=getch();
+	 outtextxy (770,770,vvod);
+
  t=0;
- while(s[i]!='\0')
- {	 y++;
- 	if (s[i]==vvod[0])
+ i=0;
+ while(Text[i]!='\0')
+ {	 
+ 	if (Text[i]==vvod[0])
  	{
- 	printf("%s",vvod);
-	 t++; 
-	 s[i]=otvet[i];
+ 	printf("%c\n",vvod[0]);
+	 t++;
+	 printf("\n q=%d j=%d\n",Chislo_simvolov,j);
+	 otvet[i]=Text[i];
 	 printf("\n");	
  	}
 	 i++;
-	
 }
 printf("Bookva %s povtorayenca %d raz\n" ,vvod,t);	
 //////////
 
  if (t==0)
- r++;
+ nevernii_otvet++;
  
  	i=0;
+ 	Chislo_simvolov=0;
+ 	int qw=0;
  	
- 	
- 
+ for (qw=0;qw<=j;qw++)
+ {
+ if (Text[qw]==otvet[qw]){
+Chislo_simvolov++;
+ printf("\n q=%d j=%d\n",Chislo_simvolov,j);
+////////////
+ wight=0;
+ settextstyle(0,0,5);
+outtextxy (35+wight,730,otvet);
+/* for (i=0;i<=qw;i++)
+ {
+ setcolor(20);
+settextstyle(0,0,5);
+outtextxy (35+wight,730,otvet);
+ wight+=30;
+///////////
+}
+*/}
+}
+
+ if (Chislo_simvolov==j){
+ //	r=13;
+  YouWin ();
+  break;	
+}
+
+
+}
 }
 getch();
 closegraph();
 return 0;
-}
 }
